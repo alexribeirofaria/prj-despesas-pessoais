6bfc1c8a9b635ce0b54c67456cd282a87246ccdb
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-07-02 17:55:41 -0300

    Add Ionic Project configurations

---
+++ AngularApp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'alexfariakof.com.mobile',
+  appName: 'despesas-frontend-mobile',
+  webDir: 'app-mobile',
+  bundledWebRuntime: false
+};
+
+export default config;
