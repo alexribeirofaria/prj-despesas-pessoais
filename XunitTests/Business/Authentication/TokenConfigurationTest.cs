b645e289498a19ddee0e8fd15dad1691d9a4b4ec
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-07-17 17:42:34 -0300

    Criação de Teste Unitério Token Configurations

--- XunitTests/Business/Authentication/TokenConfigurationTest.cs
+++ XunitTests/Business/Authentication/TokenConfigurationTest.cs
@@ -1,7 +1,26 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Despesas.Business.Authentication.Abstractions;
+using Microsoft.Extensions.Options;
 
 namespace Business.Authentication;
 public sealed class TokenConfigurationTest
 {
+    [Fact]
+    public void Properties_Should_Be_Set_Correctly()
+    {
+        // Arrange
+        var options = Options.Create(new TokenOptions
+        {
+            Issuer = "TesteIssuer",
+            Audience = "TesteAudience",
+            Seconds = 3600,
+        });
 
+        // Act
+        var tokenConfiguration = new TokenConfiguration(options);
+
+        // Assert
+        Assert.Equal("TesteAudience", tokenConfiguration.Audience);
+        Assert.Equal("TesteIssuer", tokenConfiguration.Issuer);
+        Assert.Equal(3600, tokenConfiguration.Seconds);
+    }
 }
