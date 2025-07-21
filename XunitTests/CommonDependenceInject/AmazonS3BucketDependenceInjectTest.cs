9dee4f1525693e6b887b0e1ea689ffb8cc823f19
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-07-21 19:20:09 -0300

    Criação Teste Unitário AmazonS3

---
+++ XunitTests/CommonDependenceInject/AmazonS3BucketDependenceInjectTest.cs
@@ -0,0 +1,44 @@
+﻿using Despesas.Infrastructure.Amazon;
+using Despesas.Infrastructure.Amazon.Abstractions;
+using Despesas.WebApi.CommonDependenceInject;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CommonDependenceInject;
+
+public sealed class AmazonS3BucketDependenceInjectTest
+{
+    [Fact]
+    public void AddAmazonS3BucketConfigurations_Should_Register_Required_Services()
+    {
+        // Arrange
+        var inMemorySettings = new Dictionary<string, string>
+        {
+            { "AmazonS3Configurations:AccessKey", "fake-access-key" },
+            { "AmazonS3Configurations:SecretKey", "fake-secret-key" },
+            { "AmazonS3Configurations:BucketName", "fake-bucket" }        
+        };
+
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddAmazonS3BucketConfigurations(configuration);
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Assert
+        var options = serviceProvider.GetService<IOptions<AmazonS3Options>>();
+        Assert.NotNull(options);
+        Assert.Equal("fake-access-key", options.Value.AccessKey);
+        Assert.Equal("fake-secret-key", options.Value.SecretAccessKey);
+        Assert.Equal("fake-bucket", options.Value.BucketName);
+        
+        var amazonS3Bucket = serviceProvider.GetService<IAmazonS3Bucket>();
+        Assert.NotNull(amazonS3Bucket);
+        Assert.IsType<AmazonS3Bucket>(amazonS3Bucket);
+    }
+}
