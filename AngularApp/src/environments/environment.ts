01f4f9469bbeb544bb8b412b0c9ac13849189e0b
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-08-16 07:06:56 -0300

    Atualização de de Variáveis de Ambiente

--- AngularApp/src/environments/environment.ts
+++ AngularApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  API_VERSION: "v2",
-  BASE_URL: "https://localhost:42535",
-  googleClientId: "128672837276-eovre8jcvptv0blpu5chttiv9gi7uppf.apps.googleusercontent.com"
+  production: true,
+  API_VERSION: "",
+  BASE_URL: "https://localhost:42535/api",
+  client_id: "128672837276-eovre8jcvptv0blpu5chttiv9gi7uppf.apps.googleusercontent.com"
 }
