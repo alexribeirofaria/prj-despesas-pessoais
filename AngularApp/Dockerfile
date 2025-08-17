2f4b64c1a6d64e6f8a447d6d633c67f453f4baf5
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-08-17 19:29:56 -0300

    Atualização de configurações de Ambiente v.3.0.0

--- AngularApp/Dockerfile
+++ AngularApp/Dockerfile
@@ -1,6 +1,6 @@
 FROM node:current-slim as node
 WORKDIR /app
-RUN npm install -g npm@latest >/dev/null 2ENV NODE_ENV=development
+RUN npm install -g npm@latest >/dev/null 2ENV NODE_ENV=local-development
 EXPOSE 4200
 EXPOSE 9229
 USER node
--- AngularApp/src/environments/environment.ts
+++ AngularApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: "https://localhost:42535",
+  BASE_URL: "https://alexfariakof.com/api",
   client_id: "128672837276-eovre8jcvptv0blpu5chttiv9gi7uppf.apps.googleusercontent.com"
 }
