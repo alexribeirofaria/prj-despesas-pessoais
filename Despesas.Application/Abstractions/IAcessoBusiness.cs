72c4a25ad803b6f2c15ed62b54b571d95ca452c8
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-09-04 07:27:18 -0300

    deploy/feature/feature/Aplicando-Tratamento-de-Excepitions-Globalmente

--- Despesas.Application/Abstractions/IAcessoBusiness.cs
+++ Despesas.Application/Abstractions/IAcessoBusiness.cs
@@ -6,9 +6,7 @@ namespace Despesas.Application.Abstractions;
 public interface IAcessoBusiness<DtoCa, DtoLogin> where DtoCa : class where DtoLogin : class
 {
     Task<AuthenticationDto> ValidateCredentials(DtoLogin loginDto);
-
     Task<AuthenticationDto> ValidateExternalCredentials(GoogleAuthenticationDto authenticationDto);
-    
     Task<AuthenticationDto> ValidateCredentials(string refreshToken);
     Task Create(DtoCa acessoDto);
     Task ChangePassword(Guid idUsuario, string password);
