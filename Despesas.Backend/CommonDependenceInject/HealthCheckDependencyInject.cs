8aa36186ea90f49ada2b482986961a5844f8d6b1
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-09-19 19:37:46 -0300

    Implementação de injeção do HealthChecks

--- Despesas.Backend/CommonDependenceInject/HealthCheckDependencyInject.cs
+++ Despesas.Backend/CommonDependenceInject/HealthCheckDependencyInject.cs
@@ -1,21 +1,61 @@
-﻿namespace Despesas.Backend.CommonDependenceInject;
+﻿using static Despesas.Backend.Options.HealthCheckOptions;
+
+namespace Despesas.Backend.CommonDependenceInject;
 
 public static class HealthCheckDependencyInject
 {
     public static void AddHealthCheckConfigurations(this WebApplicationBuilder builder)
     {
-        builder.Services.AddHealthChecks()
-            .AddUrlGroup(new Uri("https://alexfariakof.com"), name: "Balanço Positivo")
-            .AddUrlGroup(new Uri("https://alexfariakof.com/coveragereport"), name: "Relátorio de cobertura de códgio")
-            .AddUrlGroup(new Uri("https://alexfariakof.com:42535"), name: "Balanço Positivo Development")
-            .AddUrlGroup(new Uri("https://alexfariakof.com:42535"), name: "Doumentação API");
+
+        // Carregar configurações do appsettings
+        var healthCheckConfig = builder.Configuration
+                        .GetSection("HealthCheckOptions")
+                        .Get<HealthCheckConfig>();
+
+
+        var healthChecksBuilder = builder.Services.AddHealthChecks();
+
+        healthCheckConfig?.Endpoints?
+            .Where(e => !string.IsNullOrWhiteSpace(e.EndPoint))
+            .ToList()
+            .ForEach(e =>
+            {
+                if (e.Port is null)
+                {
+                    if (Uri.TryCreate(e.EndPoint, UriKind.Absolute, out var uri) &&
+                    (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+                    {
+                        healthChecksBuilder.AddUrlGroup(uri, name: e.Name ?? e.EndPoint);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ignorando endpoint inválido: {e.EndPoint}");
+                    }
+                }
+                else if (e.Port == 3306)
+                {
+                    if (string.IsNullOrWhiteSpace(e.User) || string.IsNullOrWhiteSpace(e.Password))
+                    {
+                        Console.WriteLine($"Ignorando banco sem usuário/senha: {e.EndPoint}");
+                        return;
+                    }
+                    var connectionString = $"Server={e.EndPoint};Port={e.Port};Database={e.Database};Uid={e.User};Pwd={e.Password};SslMode=none;";
+                    healthChecksBuilder.AddMySql(connectionString, name: e.Name ?? e.EndPoint);
+                }
+            });
+
 
         // -------------------- Health Checks UI --------------------
         builder.Services.AddHealthChecksUI(setup =>
         {
-            setup.AddHealthCheckEndpoint("Balanço Positivo Health", "/health");
+            // Executa os health checks a cada 30 minutos (1800 segundos)
+            setup.SetEvaluationTimeInSeconds(1800);
+
+            // Mantém no máximo 20 registros por endpoint para evitar excesso de memória
+            setup.MaximumHistoryEntriesPerEndpoint(20);
+
         })
-        .AddInMemoryStorage(); // Armazena resultados em memória
+        .AddInMemoryStorage();
 
     }
 }
