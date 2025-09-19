c0e52f1123583f5c537cac342eac67a798acf494
Alex Ribeiro de Faria <135660435+alexribeirofaria@users.noreply.github.com>
2025-09-19 15:20:02 -0300

    Configurações de Injeção do HelthChecks

---
+++ Despesas.Backend/CommonDependenceInject/HealthCheckDependencyInject.cs
@@ -0,0 +1,22 @@
+﻿namespace Despesas.Backend.CommonDependenceInject;
+
+public static class HealthCheckDependencyInject
+{
+    public static void AddHealthCheckConfigurations(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHealthChecks()
+            .AddUrlGroup(new Uri("https://alexfariakof.com"), name: "Balanço Positivo")
+            .AddUrlGroup(new Uri("https://alexfariakof.com/coveragereport"), name: "Relátorio de cobertura de códgio")
+            .AddUrlGroup(new Uri("https://alexfariakof.com:42535"), name: "Balanço Positivo Development")
+            .AddUrlGroup(new Uri("https://alexfariakof.com:42535"), name: "Doumentação API");
+
+        // -------------------- Health Checks UI --------------------
+        builder.Services.AddHealthChecksUI(setup =>
+        {
+            setup.AddHealthCheckEndpoint("Balanço Positivo Health", "/health");
+        })
+        .AddInMemoryStorage(); // Armazena resultados em memória
+
+    }
+}
+
